347d6d4fc7f6426316c17bf237d85bdc76e5339a
prana <prana@LAPTOP-227GK184>
2026-04-21 22:40:36 +0530

    lab_9a

---
+++ src/main/java/com/sessiontracking/SessionTrackingServlet_9a.java
@@ -0,0 +1,56 @@
+package com.sessiontracking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/SessionTracker")
+public class SessionTrackingServlet_9a extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Get or create session
+        HttpSession session = request.getSession(true);
+
+        // Session details
+        Date createTime = new Date(session.getCreationTime());
+        Date lastAccessTime = new Date(session.getLastAccessedTime());
+        String sessionId = session.getId();
+
+        // Visit count logic
+        Integer visitCount = (Integer) session.getAttribute("visitCount");
+
+        if (visitCount == null) {
+            visitCount = 1;
+        } else {
+            visitCount++;
+        }
+
+        session.setAttribute("visitCount", visitCount);
+
+        // Set response type (FIXED ERROR)
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        // HTML response
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head><title>Session Tracking Demo</title></head>");
+        out.println("<body>");
+
+        out.println("<h2>Session Tracking Information</h2>");
+        out.println("<p><b>Session ID:</b> " + sessionId + "</p>");
+        out.println("<p><b>Session Creation Time:</b> " + createTime + "</p>");
+        out.println("<p><b>Last Access Time:</b> " + lastAccessTime + "</p>");
+        out.println("<p><b>Visit Count:</b> " + visitCount + "</p>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
